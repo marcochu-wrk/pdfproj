6427b9bbc242a7218c62b62a2583850f31fc9dc1
marcochu-wrk <119313395+marcochu-wrk@users.noreply.github.com>
2024-03-04 16:58:34 +0200

    Setup

---
+++ app.py
@@ -0,0 +1,28 @@
+import spacy
+from io import StringIO
+
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+
+output_string = StringIO()
+with open('example/uiux.pdf', 'rb') as in_file:
+    parser = PDFParser(in_file)
+    doc = PDFDocument(parser)
+    rsrcmgr = PDFResourceManager()
+    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    for page in PDFPage.create_pages(doc):
+        interpreter.process_page(page)
+
+value = output_string.getvalue()
+
+def extract_names(text):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+    names = [ent.text for ent in doc.ents if ent.label_ == "PERSON "]
+
+    return names
